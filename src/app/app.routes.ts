634aa3e6215b5355dbb1d35607f8deadf4b97b77
Leonel19hub <leonel19coraite@gmail.com>
2024-08-23 23:38:46 -0300

    subir rama

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';   
 import { BodyComponent } from './components/main-page/body/body.component';
 import { EsepcialidadesPageComponent } from './components/esepcialidades-page/esepcialidades-page.component';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
 
 export const routes: Routes = [
+    {path: '', component:AdminPageComponent}
 ];
