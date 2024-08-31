577a9719a50eeae352ac2e28a37295a0408d8ac0
Ismael Ignacio Miguel Torres <ignacio25.it@gmail.com>
2024-08-31 18:55:00 -0300

    Maquetado de selección de médicos y turnos listo

--- src/app/components/form-turno/form-turno.component.ts
+++ src/app/components/form-turno/form-turno.component.ts
@@ -1,14 +1,75 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavComponent } from "../main-page/nav/nav.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form-turno',
   standalone: true,
-  imports: [RouterLink, NavComponent],
+  imports: [RouterLink, NavComponent, CommonModule],
   templateUrl: './form-turno.component.html',
   styleUrl: './form-turno.component.css'
 })
 export class FormTurnoComponent {
+  medicos = [
+    {
+      id: 1,
+      genero: "DR.",
+      nombre: "Gastón Lozano",
+      turnos: 14
+    },
+    {
+      id: 2,
+      genero: "DRA.",
+      nombre: "Alicia Gutiérrez",
+      turnos: 7
+    },
+    {
+      id: 3,
+      genero: "DR.",
+      nombre: "Ernesto Alvarado",
+      turnos: 1
+    },
+    {
+      id: 4,
+      genero: "DR.",
+      nombre: "Johnson Martínez",
+      turnos: 99
+    },
+  ];
 
+  turnos = [
+    {
+      idTurno: 1,
+      fecha: 'DD/MM/AAAA',
+      hora: 'HH:MM',
+      duracion: 30,
+      lugar: "Hospital (Presencial)",
+      consultorio: 15,
+    },
+    {
+      idTurno: 1,
+      fecha: 'DD/MM/AAAA',
+      hora: 'HH:MM',
+      duracion: 30,
+      lugar: "Hospital (Presencial)",
+      consultorio: 12,
+    },
+    {
+      idTurno: 1,
+      fecha: 'DD/MM/AAAA',
+      hora: 'HH:MM',
+      duracion: 30,
+      lugar: "Hospital (Presencial)",
+      consultorio: 12,
+    },
+    {
+      idTurno: 1,
+      fecha: 'DD/MM/AAAA',
+      hora: 'HH:MM',
+      duracion: 30,
+      lugar: "Hospital (Presencial)",
+      consultorio: 15,
+    },
+  ]
 }
