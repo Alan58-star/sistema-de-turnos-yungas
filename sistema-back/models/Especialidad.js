e77a3a6fbe86f1c372559e343fbf037f6d7d424f
Alan58-star <alanmartinez43890@gmail.com>
2024-10-03 01:58:01 -0300

    para probar

--- sistema-back/models/Especialidad.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require ('mongoose');
-const EspecialidadSchema = mongoose.Schema({
-    nombreEsp:{
-        type: String,
-        required:true
-    }
-});
-
-module.exports = mongoose.model('Especialidad',EspecialidadSchema);
