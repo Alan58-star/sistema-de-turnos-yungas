2eb82c31e6180e576b4dd572accd0cc2dcd398f4
Alan58-star <alanmartinez43890@gmail.com>
2024-10-10 03:31:52 -0300

    sin back

--- sistema-back/models/ObraSocial.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require ('mongoose');
-const ObraSocialSchema = mongoose.Schema({
-    nombreOS:{
-        type: String,
-        required:true
-    }
-});
-
-module.exports = mongoose.model('ObraSocial',ObraSocialSchema);
